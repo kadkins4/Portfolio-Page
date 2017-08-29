6f97994cb57ac3cdf292007678e616872eb4f683
Kendall <adkins.kendall90@gmail.com>
2017-08-29 13:52:24 -0400

    style fix

--- js/ka.routes.js
+++ js/ka.routes.js
@@ -14,25 +14,25 @@
       url: '/home',
       // controller: 'HomeController',
       // controllerAs: 'vm',
-      templateUrl: '/js/ng-views/home.html'
+      templateUrl: 'js/ng-views/home.html'
     })
     .state('who', {
       url: '/who',
       controller: 'WhoController',
       controllerAs: 'vm',
-      templateUrl: '/js/ng-views/who.html'
+      templateUrl: 'js/ng-views/who.html'
     })
     .state('projects', {
       url: '/projects',
       controller: 'ProjectsController',
       controllerAs: 'vm',
-      templateUrl: '/js/ng-views/projects.html'
+      templateUrl: 'js/ng-views/projects.html'
     })
     .state('cv', {
       url: '/cv',
       controller: 'CvController',
       controllerAs: 'vm',
-      templateUrl: '/js/ng-views/cv.html'
+      templateUrl: 'js/ng-views/cv.html'
     })
     $urlRouterProvider.otherwise('/home')
   }
