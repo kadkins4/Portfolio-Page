118e8ae65b5c5d59bfcf2b606e52e4bc57f29c15
Kendall Adkins <adkins.kendall90@gmail.com>
2018-08-03 17:41:13 -0400

    setup client server

---
+++ server/src/app.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const morgan = require('morgan')
+
+const app = express()
+app
+    .use(morgan('combined'))
+    .use(bodyParser.json())
+    .use(cors())
+    .get('/status', (req, res) => {
+        res.send({
+            message: 'hello'
+        })
+    })
+    .listen(process.env.PORT || 8081)
