fc7feef875d0b614949ebed7fae77aa13cb87b74
Kendall Adkins <adkins.kendall90@gmail.com>
2018-10-03 00:47:10 -0400

    attempt 3 to deploy

--- portfolio/src/main.js
+++ portfolio/src/main.js
@@ -3,12 +3,6 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
-module.exports = {
-  baseUrl: process.env.NODE_ENV === 'production'
-    ? '/Portfolio-Page/'
-    : '/'
-}
-
 export const eventBus = new Vue()
 
 new Vue({
